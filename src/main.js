19a2d630538932a958cbefa850e582d27d6c0f48
asimaahmood <aabuashar@gmail.com>
2024-05-01 20:08:22 +0500

    gsap addedd

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import NavBar from './components/NavBar.vue';
+import moment from 'moment';
 
 const app = createApp(App);
 
+app.config.globalProperties.$moment = moment;
 app.component('nav-bar', NavBar);
 app.use(router);
 app.mount('#app');
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -2,8 +2,8 @@
 
 <template>
 <div class="home-view-background">
-    <div class="flex justify-center pt-4">
-        <div class="border rounded-lg container backdrop-blur-lg border-stone-400 p-3 bg-slate-300 shadow-lg">
+    <div class="flex justify-center pt-4 ">
+        <div class="border rounded-lg container  border-stone-400 p-3 bg-slate-300 shadow-lg animate ">
             <div class="grid grid-cols-1">
                 <h1 class="text-black text-center mb-5 text-2xl"><i class="fa-solid fa-smog"></i> Find Out Current Weather</h1>
                 <input type="text" v-model="location" placeholder="Enter location" @keypress.enter="fetchWeather" class="w-100 px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:border-blue-200 focus:ring-1 focus:ring-blue-200 text-black shadow-md">
@@ -16,29 +16,34 @@
                     {{ locationErrorMessage }}
                 </p>
             </div>
-            <div class="grid grid-cols-1 p-2 border-b mb-3 border-dark" v-if="weatherData" >
+            <div class="grid grid-cols-1 p-2 border-b mb-3 border-dark" v-if="weatherData">
                 <p><i class="fa-solid fa-house-chimney"></i> {{ weatherData.location.name }}, {{ weatherData.location.region }},{{ weatherData.location.country }}</p>
             </div>
-            <div  v-if="weatherData">
-                <div class="flex items-center">
-                    <img :src="weatherData.current.condition.icon" class="weather-image" />
-                    <h1 class="mb-0 weather-temp-h1 pl-3"> {{ weatherData.current.temp_c }}°C </h1>
-                    <div class=" pl-8">
-                        <div class="flex items-center">
-                            <h5 class="mb-2">{{ weatherData.current.condition.text}}</h5> 
-                        </div>
-                        <div class="flex items-center">
-                            <h5>Precipitation</h5>
-                            <h5 class="pl-3">{{ weatherData.current.precip_in}} <sub>mm</sub></h5> 
+            <div v-if="weatherData">
+                <div class="flex justify-between items-center">
+                    <div class="flex items-center">
+                        <img :src="weatherData.current.condition.icon" class="weather-image" />
+                        <h1 class="mb-0 weather-temp-h1 pl-3"> {{ weatherData.current.temp_c }}°C </h1>
+                        <div class=" pl-8">
+                            <div class="flex items-center">
+                                <h5 class="mb-2">{{ weatherData.current.condition.text}}</h5>
+                            </div>
+                            <div class="flex items-center">
+                                <h5>Precipitation</h5>
+                                <h5 class="pl-3">{{ weatherData.current.precip_in}} <sub>mm</sub></h5>
+                            </div>
                         </div>
                     </div>
+                    <div class="border rounded-full shadow-lg border-blue-800 bg-violet-300 px-8 py-2 text-white mr-5 opacity-25">
+                        <h5><i class="fa-solid fa-calendar-days pr-3"></i> {{ formattedDate }}</h5>
+                        <h5><i class="fa-solid fa-clock pr-3"></i> {{ formattedTime }}</h5>
+                    </div>
                 </div>
-                <!-- <p>Local Time: <i class="fas fa-clock"></i> {{ weatherData.current.last_updated }}</p> -->
             </div>
-           
+
             <div class="grid grid-cols-5 gap-4 text-center" v-if="weatherData">
                 <div class="p-4">
-                    <div  class="weather-card-body">
+                    <div class="weather-card-body">
                         <i class="fas fa-temperature-low "></i>
                         <div class="pl-3">
                             <span>Feels Like </span>
@@ -47,11 +52,11 @@
                     </div>
                 </div>
                 <div class="p-4">
-                    <div  class="weather-card-body">
+                    <div class="weather-card-body">
                         <i class="fas fa-wind"></i>
                         <div class="pl-3">
                             <span>Wind Speed</span>
-                            <h5>{{ weatherData.current.wind_kph}} <sub>Kmph</sub></h5> 
+                            <h5>{{ weatherData.current.wind_kph}} <sub>Kmph</sub></h5>
                         </div>
                     </div>
                 </div>
@@ -60,26 +65,26 @@
                         <i class="fas fa-location-arrow"></i>
                         <div class="pl-3">
                             <span>Wind Direction</span>
-                            <h5> {{ weatherData.current.wind_dir}}</h5> 
+                            <h5> {{ weatherData.current.wind_dir}}</h5>
                         </div>
                     </div>
                 </div>
                 <div class="p-4">
-                    <div  class="weather-card-body">
+                    <div class="weather-card-body">
                         <i class="far fa-compass"></i>
                         <div class="pl-3">
                             <span>Wind Degree</span>
-                            <h5>{{ weatherData.current.wind_degree}}°</h5> 
+                            <h5>{{ weatherData.current.wind_degree}}°</h5>
                         </div>
                     </div>
                 </div>
                 <div class="p-4">
-                    <div  class="weather-card-body">
+                    <div class="weather-card-body">
                         <i class="fas fa-arrow-alt-circle-down"></i>
                         <div class="pl-3 text-center">
                             <div>
                                 <span>Wind Pressure </span>
-                                <h5>{{ weatherData.current.pressure_in}} <sub>mm</sub></h5> 
+                                <h5>{{ weatherData.current.pressure_in}} <sub>mm</sub></h5>
                             </div>
                         </div>
                     </div>
@@ -87,7 +92,7 @@
             </div>
             <div class="grid grid-cols-5 gap-2 text-center" v-if="weatherData">
                 <div class="p-4">
-                    <div  class="weather-card-body">
+                    <div class="weather-card-body">
                         <i class="fas fa-tint"></i>
                         <div class="pl-3">
                             <span>Humidity </span>
@@ -96,11 +101,11 @@
                     </div>
                 </div>
                 <div class="p-4">
-                    <div  class="weather-card-body">
+                    <div class="weather-card-body">
                         <i class="fas fa-cloud"></i>
                         <div class="pl-3">
                             <span>Cloud Cover</span>
-                            <h5>{{ weatherData.current.cloud}}%</h5> 
+                            <h5>{{ weatherData.current.cloud}}%</h5>
                         </div>
                     </div>
                 </div>
@@ -109,16 +114,16 @@
                         <i class="fas fa-smog"></i>
                         <div class="pl-3">
                             <span>Visibility</span>
-                            <h5> {{ weatherData.current.vis_km}} <sub>km</sub></h5> 
+                            <h5> {{ weatherData.current.vis_km}} <sub>km</sub></h5>
                         </div>
                     </div>
                 </div>
                 <div class="p-4">
-                    <div  class="weather-card-body">
+                    <div class="weather-card-body">
                         <i class="fas fa-tachometer-alt"></i>
                         <div class="pl-3">
                             <span>UV Index</span>
-                            <h5>{{ weatherData.current.uv}}</h5> 
+                            <h5>{{ weatherData.current.uv}}</h5>
                         </div>
                     </div>
                 </div>
@@ -127,7 +132,7 @@
                         <i class="fas fa-arrow-alt-circle-down"></i>
                         <div class="pl-3">
                             <span>Wind Gust</span>
-                            <h5>{{ weatherData.current.gust_kph}} km/h</h5> 
+                            <h5>{{ weatherData.current.gust_kph}} km/h</h5>
                         </div>
                     </div>
                 </div>
@@ -139,11 +144,13 @@
 
 <script>
 import {
-    ref
+    ref,
+    computed,
+    onMounted
 } from 'vue';
+import moment from 'moment';
 import WeatherService from '../services/WeatherService';
-// import HelloWorld from '../components/HelloWorld.vue';
-
+import { gsap } from 'gsap';
 export default {
     name: 'Weather',
     setup() {
@@ -151,6 +158,32 @@ export default {
         const weatherData = ref('');
         const loading = ref(false);
         const locationWarningMessage = "Please select a location";
+
+        const formattedDate = computed(() => {
+            if (weatherData.value.current.last_updated) {
+                return moment(weatherData.value.current.last_updated).format('DD-MM-YYYY');
+            } else {
+                return '';
+            }
+        });
+
+       onMounted(() => {
+            console.log ('animate')
+           // Use GSAP to animate the background element
+           gsap.from('.animate', { duration: 1, scale: 0, opacity: 0, ease: customEase });
+        });
+        const customEase = (x) => {
+        // Custom ease function for smoother animation
+            return 1 - Math.pow(1 - x, 1);
+        };
+        const formattedTime = computed(() => {
+            if (weatherData.value.current.last_updated) {
+                return moment(weatherData.value.current.last_updated).format('h:mm a');
+            } else {
+                return '';
+            }
+        });
+
         const fetchWeather = async () => {
             if (!location.value.trim()) {
                 console.warn(locationWarningMessage);
@@ -168,19 +201,22 @@ export default {
                 console.error("Error fetching weather data:", error);
             } finally {
                 loading.value = false;
-            }
+            } 
         };
-
 
         return {
             location,
             weatherData,
             loading,
             fetchWeather,
-            locationWarningMessage
+            locationWarningMessage,
+            formattedDate,
+            formattedTime,
+            
 
         };
-    }
+    },
+   
 };
 </script>
 
@@ -192,28 +228,33 @@ export default {
     height: 100vh;
     background-image: url('/images/weather-image.jpg');
 }
-.weather-temp-h1{
-    font-size:4rem;
+
+.weather-temp-h1 {
+    font-size: 4rem;
 }
-.weather-image{
+
+.weather-image {
     width: auto;
     height: 143px;
 }
-.weather-card-body{
-    background-color: rgba(0, 140, 255, 0.429);
-    border: 2px solid rgb(0, 89, 255);
+
+.weather-card-body {
+    background-color: rgba(0, 166, 255, 0.429);
+    border: 2px solid rgb(0, 35, 110);
     border-radius: 10px;
     box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.419);
     padding: 20px;
     margin: 0 5px;
     display: flex;
 }
-.weather-card-body i{
+
+.weather-card-body i {
     font-size: 3rem;
-    color: rgb(0, 68, 255);
+    color: rgba(0, 68, 255, 0.648);
     text-shadow: 1px 1px rgba(255, 255, 255, 0.167);
 }
-h5{
+
+h5 {
     font-size: 1.2rem;
 }
 </style>
